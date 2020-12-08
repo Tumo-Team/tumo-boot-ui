2eb2decb1320e1277691de8384ffdd6765ac706c
tycoding <tycoding@sina.com>
2020-12-08 12:32:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <a-config-provider :locale="locale">
+      <router-view />
+    </a-config-provider>
+  </div>
+</template>
+
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      locale: zhCN
+    }
+  }
+}
+</script>
