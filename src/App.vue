82c5908d7f21dc4257ae60294c61a92b9621d175
TyCoding <tycoding@sina.com>
2022-02-24 18:00:13 +0800

    fix: 增加水印

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,19 @@
   import { AppProvider } from '/@/components/Application';
   import { useTitle } from '/@/hooks/web/useTitle';
   import { useLocale } from '/@/locales/useLocale';
+  import { useWatermark } from '/@/hooks/web/useWatermark';
 
   import 'dayjs/locale/zh-cn';
+  import { onMounted } from 'vue';
   // support Multi-language
   const { getAntdLocale } = useLocale();
 
   // Listening to page changes and dynamically changing site titles
   useTitle();
+
+  // 全局水印，请自行删除
+  const { setWatermark } = useWatermark();
+  onMounted(() => {
+    setWatermark('Tumo Team');
+  });
 </script>
